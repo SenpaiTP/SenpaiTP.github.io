fb642c19c1302b6408c44ebc8196934e23dabe20
SenpaiTP <thomascool01@gmail.com>
2024-12-20 20:03:51 +0100

    Second Try should work

--- githubpage/src/index.js
+++ githubpage/src/index.js
@@ -6,9 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
       <App />
-      <h1>Hello World</h1>
-    </div>
   </React.StrictMode>
 );
